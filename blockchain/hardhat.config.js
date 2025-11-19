f6bf6fc34696d46067b6b187676af3598da65e9f
Salvador Depons <salva.depons.18@gmail.com>
2025-11-19 09:05:17 -0300

    refactor test

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -5,6 +5,7 @@ require("hardhat-coverage");
 module.exports = {
   solidity: "0.8.20",
   networks: {
+<<<<<<< Updated upstream
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL,
       accounts: [process.env.PRIVATE_KEY]
@@ -12,5 +13,10 @@ module.exports = {
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY
+=======
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+    },
+>>>>>>> Stashed changes
   }
 };
